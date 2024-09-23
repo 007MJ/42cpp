0245560ed22c6f30de9d60a1080a75c3fd182a1e
007MJ <k.mijiza@gmail.com>
2024-09-23 18:03:40 -0400

    finish the module 7

---
+++ module1/module7/ex02/Array.hpp
@@ -0,0 +1,77 @@
+#ifndef ARRAY_HPP 
+#define ARRAY_HPP
+#include <iostream>
+
+template <typename T>
+class Array {
+    private:
+        T *array;
+        unsigned int  len;
+    public:
+        Array(){
+            this->array = 0;
+            this->len = 0;
+        }
+
+        Array(const Array &ob) {
+            *this = ob;
+        }
+
+        Array(unsigned int n) : len(n){
+            this->array = new T[len];
+            unsigned int  i = 0;
+            while (i < this->len){
+                this->array[i] = 0; 
+                i++;
+            }
+        }
+
+        Array& operator=(const Array &ob){
+            this->len = ob.size();
+            this->array = new T[this->len];
+            unsigned int  i = 0;
+            while (i < this->len){
+                this->array[i] = ob.getIndex(i);
+                i++;
+            }
+            return (*this);
+        }
+
+         void acces(unsigned int index){
+            try{
+                if (index > this->len || this->len <= 0)
+                    throw 0;
+                else
+                    std::cout<< "Acces to index : [" <<  this->array[index] << "]"<< std::endl;
+            }catch(int i){
+                std::cout << "You are not in the range " << std::endl;
+            }
+        }
+        unsigned int  size() const {return this->len;}
+
+        T  getIndex(unsigned int index) const {
+            try{
+                if (index > this->len && this->len > 0)
+                    throw 0;
+                else
+                    return (this->array[index]);
+            }catch(int i){
+                std::cout << "You are not in the range " << std::endl;
+            }
+            return 0;
+        }
+        void add(T var, unsigned int index){
+            try{
+                if (index > this->len && this->len > 0)
+                    throw 0;
+                else
+                    (this->array[index] = var);
+            }catch(int i){
+                std::cout << "You are not in the range " << std::endl;
+            }
+        }
+        ~Array() {
+            delete[] this->array;
+        }
+};
+#endif
